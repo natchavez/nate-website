4fff316848b9edefef5e7f8da97eab2acc7df3ef
natchavez <nathanielbchavez@gmail.com>
2022-02-15 12:04:02 +0800

    react

---
+++ src/components/Contact/ButtonBW.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ButtonBW = () => {
+  return <div>ButtonBW</div>;
+};
+
+export default ButtonBW;
---
+++ src/components/Main/PersonalImage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PersonalImage = () => {
+  return <div>PersonalImage</div>;
+};
+
+export default PersonalImage;
